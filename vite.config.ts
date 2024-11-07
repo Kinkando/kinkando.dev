79f4e29bf6c49b48ff8ccd6e3e8026e4e707211c
Kinkando <TY_Sage@hotmail.com>
2024-11-07 16:25:06 +0700

    chore(refactor): remove unused code

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { resolve } from 'path';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-
-	resolve: {
-		alias: {
-			'@': resolve(__dirname, 'src')
-		}
-	},
 
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']

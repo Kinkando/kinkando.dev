28316f38201c94223b4b4bdf81a72ac89f77caef
Dumbbell <73744120+Kinkando@users.noreply.github.com>
2026-06-14 15:04:42 +0700

    chore: disable Register Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import NavBar from './components/NavBar'
 import TopLoader from './components/TopLoader'
 import PortfolioPage from './pages/PortfolioPage'
 import LoginPage from './pages/LoginPage'
-import RegisterPage from './pages/RegisterPage'
+// import RegisterPage from './pages/RegisterPage'
 import AccountPage from './pages/AccountPage'
 import NotificationsPage from './pages/NotificationsPage'
 import FinancePage from './pages/FinancePage'
@@ -62,7 +62,7 @@ export default function App() {
         />
         <Route path="/news" element={<NewsPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
+        {/* <Route path="/register" element={<RegisterPage />} /> */}
         <Route
           path="/account"
           element={
